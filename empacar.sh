adc068ea2b80e9f7e0aa696076dc71e5f17544a3
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-04-19 12:46:24 -0400

    Ahora se pueden empacar y copiar a emacs desde poneren.

--- empacar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Este script es para empacar todos los snippets para distribuirlos si es necesario.
-
-# Este script asume que está siendo ejecutado desde el directorio raíz del proyecto.
-
-zip -r -x@nocopiar.txt pablo-snippets.zip ./
