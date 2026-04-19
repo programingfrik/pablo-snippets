0b6d76ef6b9db3282d309c59b28103f108fe15bc
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-04-19 10:57:50 -0400

    Ahora copiar-a-emacs.sh puede ser invocado desde cualquier ruta.

--- copiar-a-emacs.sh
+++ copiar-a-emacs.sh
@@ -2,6 +2,16 @@
 
 # Este script es para mandar los snippets al directorio de emacs.
 
-# Este script asume que está siendo ejecutado desde el directorio raíz del proyecto.
+# Busca la ruta del directorio del proyecto
+echo $0
+dirpro=`echo $0 | sed "s/\(.\+\)\?copiar-a-emacs.sh/\1/"`
 
-rsync -truv --exclude-from=nocopiar.txt ./ ~/.emacs.d/snippets
+# Si dirpro está vacio porque pwd es el directorio del proyecto, ponle
+# una referencia al directorio actual para que el parámetro origen del
+# rsync no esté vacío.
+if [[ $dirpro = "" ]] then
+   dirpro="./"
+fi
+
+# Sincroniza el directorio del proyecto con el directorio de los snippets de emacs.
+rsync -truv --exclude-from=${dirpro}nocopiar.txt ${dirpro} ~/.emacs.d/snippets
